d1fd731d7796e9e73b8ed32df3eac253d0ff7bec
rni22 <rahulair@yahoo.com>
2022-07-08 09:38:12 +0000

    Fix indentation

--- main.py
+++ main.py
@@ -4,243 +4,243 @@ import random
 import pickle
 
 class WordleBot:
-    def get_word_list(self):
-      with open("./word_list.pkl", "rb") as f:
-        return pickle.load(f)
-  
-    def __init__(self):
-      self.word_list = self.get_word_list()
-      self.letter2word = {}
-      self.letterpos2word = {}
-      self.letterfreq2word = {}
+  def get_word_list(self):
+    with open("./word_list.pkl", "rb") as f:
+      return pickle.load(f)
 
-      with open("./word_frequencies.pkl", "rb") as f:
-        self.word_frequencies = pickle.load(f)
+  def __init__(self):
+    self.word_list = self.get_word_list()
+    self.letter2word = {}
+    self.letterpos2word = {}
+    self.letterfreq2word = {}
+
+    with open("./word_frequencies.pkl", "rb") as f:
+      self.word_frequencies = pickle.load(f)
+      
+    self.reset_state()
         
-      self.reset_state()
-          
-    def reset_state(self):
-        self.candidate_set = self.word_list.copy()
+  def reset_state(self):
+      self.candidate_set = self.word_list.copy()
 
-    def process_letter2word(self, letter, word):
-      if letter not in self.letter2word:
-        self.letter2word[letter] = set()
+  def process_letter2word(self, letter, word):
+    if letter not in self.letter2word:
+      self.letter2word[letter] = set()
 
-      self.letter2word[letter].add(word)
+    self.letter2word[letter].add(word)
 
-    def process_letterpos2word(self, letter, word, pos):
-      if letter not in self.letterpos2word:
-          self.letterpos2word[letter] = {}
+  def process_letterpos2word(self, letter, word, pos):
+    if letter not in self.letterpos2word:
+        self.letterpos2word[letter] = {}
 
-      if pos not in self.letterpos2word[letter]:
-          self.letterpos2word[letter][pos] = set()
+    if pos not in self.letterpos2word[letter]:
+        self.letterpos2word[letter][pos] = set()
 
-      self.letterpos2word[letter][pos].add(word)
+    self.letterpos2word[letter][pos].add(word)
 
-    def process_letterfreq2word(self, word):
-      letter_freq = self.generate_letter_freq(word)
+  def process_letterfreq2word(self, word):
+    letter_freq = self.generate_letter_freq(word)
 
-      for letter in letter_freq:
-        freq = letter_freq[letter]
-        if letter not in self.letterfreq2word:
-          self.letterfreq2word[letter] = {}
+    for letter in letter_freq:
+      freq = letter_freq[letter]
+      if letter not in self.letterfreq2word:
+        self.letterfreq2word[letter] = {}
 
-        if freq not in self.letterfreq2word[letter]:
-          self.letterfreq2word[letter][freq] = set()
+      if freq not in self.letterfreq2word[letter]:
+        self.letterfreq2word[letter][freq] = set()
 
-        self.letterfreq2word[letter][freq].add(word)
-  
-    def process_word_list(self):
-        for word in self.word_list:
-            for i in range(len(word)):
-                letter = word[i]
-                self.process_letter2word(letter, word)              
-                self.process_letterpos2word(letter, word, i)
+      self.letterfreq2word[letter][freq].add(word)
 
-            self.process_letterfreq2word(word)
-  
-    def handle_green(self, letter, pos):
-      candidate_set = self.word_list.copy()
+  def process_word_list(self):
+      for word in self.word_list:
+          for i in range(len(word)):
+              letter = word[i]
+              self.process_letter2word(letter, word)              
+              self.process_letterpos2word(letter, word, i)
 
-      candidate_set &= self.letterpos2word[letter][pos]
+          self.process_letterfreq2word(word)
 
-      return candidate_set
+  def handle_green(self, letter, pos):
+    candidate_set = self.word_list.copy()
 
-    def handle_yellow(self, letter, pos):
-      candidate_set = set()
+    candidate_set &= self.letterpos2word[letter][pos]
 
-      for i in range(0, 5):
-        if i != pos and i in self.letterpos2word[letter]:
-          candidate_set |= self.letterpos2word[letter][i]
+    return candidate_set
 
-      return candidate_set
+  def handle_yellow(self, letter, pos):
+    candidate_set = set()
 
-    def handle_black(self, letter, processing_state):
-      candidate_set = self.word_list.copy()
+    for i in range(0, 5):
+      if i != pos and i in self.letterpos2word[letter]:
+        candidate_set |= self.letterpos2word[letter][i]
 
-      if letter not in processing_state:
-        candidate_set -= self.letter2word[letter]
+    return candidate_set
+
+  def handle_black(self, letter, processing_state):
+    candidate_set = self.word_list.copy()
+
+    if letter not in processing_state:
+      candidate_set -= self.letter2word[letter]
+    else:
+      freq = processing_state[letter]
+      candidate_set &= self.letterfreq2word[letter][freq]
+
+    return candidate_set
+
+  def get_processing_state(self, guess, result):
+    processing_state = {}
+
+    for i in range(0, len(guess)):
+      if result[i] == 'G' or result[i] == 'Y':
+        if guess[i] not in processing_state:
+          processing_state[guess[i]] = 1
+        else:
+          processing_state[guess[i]] += 1
+
+    return processing_state
+
+  def update_candidate_set(self, guess, result):
+    for i in range(0, len(guess)):
+      if result[i] == 'G':
+        self.candidate_set &= self.handle_green(guess[i], i)
+      elif result[i] == 'Y':
+        self.candidate_set &= self.handle_yellow(guess[i], i)
       else:
-        freq = processing_state[letter]
-        candidate_set &= self.letterfreq2word[letter][freq]
+        self.candidate_set &= self.handle_black(
+          guess[i],
+          self.get_processing_state(guess, result)
+        )
 
-      return candidate_set
+    self.candidate_set.discard(guess)
+    
+    assert len(self.candidate_set) != 0
 
-    def get_processing_state(self, guess, result):
-      processing_state = {}
+  def generate_letter_freq(self, word):
+    letter_freq = {}
 
-      for i in range(0, len(guess)):
-        if result[i] == 'G' or result[i] == 'Y':
-          if guess[i] not in processing_state:
-            processing_state[guess[i]] = 1
+    for i in range(len(word)):
+      if word[i] in letter_freq:
+        letter_freq[word[i]] += 1
+      else:
+        letter_freq[word[i]] = 1
+
+    return letter_freq
+    
+  def evaluate_guess(self, word, guess):
+      result = ""
+      letter_freq = self.generate_letter_freq(word)
+    
+      for i in range(len(guess)):
+          if guess[i] not in word or letter_freq[guess[i]] == 0:
+              result += 'B'
+          elif guess[i] == word[i]:
+              result += 'G'
+              letter_freq[guess[i]] -= 1
           else:
-            processing_state[guess[i]] += 1
+              result += 'Y'
+              letter_freq[guess[i]] -= 1
 
-      return processing_state
+      return result
 
-    def update_candidate_set(self, guess, result):
-      for i in range(0, len(guess)):
-        if result[i] == 'G':
-          self.candidate_set &= self.handle_green(guess[i], i)
-        elif result[i] == 'Y':
-          self.candidate_set &= self.handle_yellow(guess[i], i)
-        else:
-          self.candidate_set &= self.handle_black(
-            guess[i],
-            self.get_processing_state(guess, result)
-          )
+  def pick_best_word(self):
+      best_frequency = -1
+      best_word = None
 
-      self.candidate_set.discard(guess)
-      
-      assert len(self.candidate_set) != 0
+      for word in self.candidate_set:
+          if self.word_frequencies[word] > best_frequency:
+              best_frequency = self.word_frequencies[word]
+              best_word = word
 
-    def generate_letter_freq(self, word):
-      letter_freq = {}
+      return best_word
 
-      for i in range(len(word)):
-        if word[i] in letter_freq:
-          letter_freq[word[i]] += 1
-        else:
-          letter_freq[word[i]] = 1
+  def play(self, word, verbose=False):
+      guess = "soare"
+      turns = 1
 
-      return letter_freq
-      
-    def evaluate_guess(self, word, guess):
-        result = ""
-        letter_freq = self.generate_letter_freq(word)
-      
-        for i in range(len(guess)):
-            if guess[i] not in word or letter_freq[guess[i]] == 0:
-                result += 'B'
-            elif guess[i] == word[i]:
-                result += 'G'
-                letter_freq[guess[i]] -= 1
-            else:
-                result += 'Y'
-                letter_freq[guess[i]] -= 1
+      if verbose == True:
+          print("Word: %s" % word)
+          print("Guess: %s" % guess)
 
-        return result
+      while True:
+          if word == None:
+              result = input("enter result: ")
+              result = result.upper().strip()
+          else:
+              result = self.evaluate_guess(word, guess)
 
-    def pick_best_word(self):
-        best_frequency = -1
-        best_word = None
+          if result == "GGGGG":
+            return turns
 
-        for word in self.candidate_set:
-            if self.word_frequencies[word] > best_frequency:
-                best_frequency = self.word_frequencies[word]
-                best_word = word
+          self.update_candidate_set(guess, result)
 
-        return best_word
+          guess = self.pick_best_word()
 
-    def play(self, word, verbose=False):
-        guess = "soare"
-        turns = 1
+          if verbose == True:
+              print("Next guess: %s" % guess)
 
-        if verbose == True:
-            print("Word: %s" % word)
-            print("Guess: %s" % guess)
+          turns += 1
 
-        while True:
-            if word == None:
-                result = input("enter result: ")
-                result = result.upper().strip()
-            else:
-                result = self.evaluate_guess(word, guess)
+  def interactive_play(self):
+      return self.play(None, True)
 
-            if result == "GGGGG":
-              return turns
+  def test_preprocessing(self):
+      word = "abbey"
 
-            self.update_candidate_set(guess, result)
-
-            guess = self.pick_best_word()
-
-            if verbose == True:
-                print("Next guess: %s" % guess)
-
-            turns += 1
-
-    def interactive_play(self):
-        return self.play(None, True)
-
-    def test_preprocessing(self):
-        word = "abbey"
-
-        print("abbey in letter2word['a']: %s" % str(word in self.letter2word['a']))
-        print("abbey in letter2word['b']: %s" % str(word in self.letter2word['b']))
-        print("abbey in letter2word['x']: %s" % str(word in self.letter2word['x']))
-        print("abbey in letterpos2word['b'][1]: %s" % str(word in self.letterpos2word['b'][1]))
-        print("abbey in letterpos2word['b'][2]: %s" % str(word in self.letterpos2word['b'][2]))
-        print("abbey in letterpos2word['b'][3]: %s" % str(word in self.letterpos2word['b'][3]))
+      print("abbey in letter2word['a']: %s" % str(word in self.letter2word['a']))
+      print("abbey in letter2word['b']: %s" % str(word in self.letter2word['b']))
+      print("abbey in letter2word['x']: %s" % str(word in self.letter2word['x']))
+      print("abbey in letterpos2word['b'][1]: %s" % str(word in self.letterpos2word['b'][1]))
+      print("abbey in letterpos2word['b'][2]: %s" % str(word in self.letterpos2word['b'][2]))
+      print("abbey in letterpos2word['b'][3]: %s" % str(word in self.letterpos2word['b'][3]))
 
 def evaluate_solution(bot, word_list, print_failed_words=True, verbose=False):
-    sum = 0.0
-    histogram = {}
-    failed_words = set()
+  sum = 0.0
+  histogram = {}
+  failed_words = set()
 
-    for word in word_list:
-        bot.reset_state()
-        turns = bot.play(word, verbose)
+  for word in word_list:
+      bot.reset_state()
+      turns = bot.play(word, verbose)
 
-        if turns > 6:
-            failed_words.add(word)
+      if turns > 6:
+          failed_words.add(word)
 
-        sum += turns
-        if turns not in histogram:
-            histogram[turns] = 1
-        else:
-            histogram[turns] += 1
+      sum += turns
+      if turns not in histogram:
+          histogram[turns] = 1
+      else:
+          histogram[turns] += 1
 
-    for key in histogram:
-        print("%d turns: %d" % (key, histogram[key]))
+  for key in histogram:
+      print("%d turns: %d" % (key, histogram[key]))
 
-    print("Average number of turns: %f" % (sum / len(word_list)))
+  print("Average number of turns: %f" % (sum / len(word_list)))
 
-    if print_failed_words:
-        print("Failed words: ")
-        for word in failed_words:
-            bot.reset_state()
-            turns = bot.play(word, True)
-            print("Number of turns: %d" % turns)
+  if print_failed_words:
+      print("Failed words: ")
+      for word in failed_words:
+          bot.reset_state()
+          turns = bot.play(word, True)
+          print("Number of turns: %d" % turns)
 
 def get_wordle_list():
-    wordles = set()
-    with open("./wordles.txt") as f:
-        lines = f.readlines()
+  wordles = set()
+  with open("./wordles.txt") as f:
+      lines = f.readlines()
 
-        for word in lines:
-            word = word.strip()
-            word = word.lower()
+      for word in lines:
+          word = word.strip()
+          word = word.lower()
 
-            wordles.add(word)
+          wordles.add(word)
 
-    return wordles
+  return wordles
 
 if __name__ == "__main__":
-    random.seed()
+  random.seed()
 
-    bot = WordleBot()
-    bot.process_word_list()
+  bot = WordleBot()
+  bot.process_word_list()
 
-#    bot.play("abyss", True)
-#    evaluate_solution(bot, get_wordle_list())
-    bot.interactive_play()
+#  bot.play("abyss", True)
+  evaluate_solution(bot, get_wordle_list())
+#  bot.interactive_play()
